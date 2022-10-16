bd27fe1e87d839bff47cb1ef72d2e2003369dcd4
beijaflor <sho.otani@gmail.com>
2022-10-16 16:04:30 +0900

    create app to convert decimal to hex converter

---
+++ src/components/input.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let label: string;
+	export let value: number;
+	export let decoder: (value: number) => string;
+	export let encoder: (value: string) => number;
+
+	let innerValue: string;
+	const dispatch = createEventDispatcher();
+
+	const inputHandler = (v: Event & { currentTarget: HTMLInputElement }) => {
+		try {
+			if (v.currentTarget.value === '') {
+				v.currentTarget.value = decoder(0);
+				dispatch('update', { value: 0 });
+			} else {
+				const num = encoder(v.currentTarget.value);
+				if (num.toString() !== v.currentTarget.value) {
+					v.currentTarget.value = num.toString();
+				}
+				dispatch('update', { value: num });
+			}
+		} catch (e) {
+			v.currentTarget.value = decoder(value);
+			console.info(e);
+		}
+	};
+
+	$: if (innerValue !== decoder(value)) {
+		console.log('watch');
+		innerValue = decoder(value);
+	}
+</script>
+
+<label class="input-component">
+	<span class="label-text">{label}</span>
+	<input class="input" value={decoder(value)} on:input={inputHandler} data-testid={label} />
+</label>
+
+<style>
+	.input-component {
+		display: flex;
+		align-items: center;
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 10px;
+	}
+	.label-text {
+		flex-basis: 40%;
+		text-transform: uppercase;
+	}
+	.input {
+		font-family: monospace;
+		flex-basis: 60%;
+		border: solid 1px #ddd;
+		border-radius: 4px;
+		padding: 4px 8px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Input from '../components/input.svelte';
+
+	let value: number = 0;
+
+	const fields = [
+		{
+			label: 'decimal',
+			decoder: (v: number) => v.toString(),
+			encoder: (v: string) => {
+				const ret = Number(v);
+				if (Number.isNaN(ret)) {
+					throw new Error('parse error');
+				}
+				return ret;
+			}
+		},
+		{
+			label: 'hex',
+			decoder: (v: number) => v.toString(16).toUpperCase(),
+			encoder: (v: string) => {
+				const ret = parseInt(v, 16);
+				if (Number.isNaN(ret)) {
+					throw new Error('parse error');
+				}
+				return ret;
+			}
+		}
+	];
+
+	$: console.log(value);
+</script>
+
+<section class="page">
+	<h1 class="page-heading">HEX DECIMAL CONVERTER</h1>
+	{#each fields as field}
+		<Input
+			{...field}
+			{value}
+			on:update={(v) => {
+				console.log(v);
+				value = v.detail.value;
+			}}
+		/>
+	{/each}
+</section>
+
+<style>
+	.page-heading {
+		font-weight: bold;
+		font-size: 2.4rem;
+		margin: 0 0 40px;
+	}
+	.page {
+		padding: 40px;
+	}
+</style>
